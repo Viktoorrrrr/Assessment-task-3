6dca4273f8a97db311ab7babe5d4f5e1fc275940
Viktoorrrrr <victor.chen11@education.nsw.gov.au>
2026-06-19 21:01:01 +1000

    finsihed item system and made a little menu for players

--- main.py
+++ main.py
@@ -22,12 +22,31 @@ def introduction():
         player = {'name': name,'health': 100, 'position': [0, 0], 'attack': 40, 'healthpotion': 5, 'inventory': [], 'max_health': 100}
     return player
 
-def terminal(text, speed=0.05): # function was entirely AI
+def terminal(text, speed=0.0005): # function was entirely AI
     for char in text:
         sys.stdout.write(char)
         sys.stdout.flush()
         time.sleep(speed)
     print() # Prints a newline at the end
+def menu(player):
+    print('MENU')
+    print('Type I for inventory access')
+    print('Type S to save game')     
+    print('Type E to exit game (please save first!!)')       
+    valid = False
+    while not valid:
+        p_input = input('Type the correspondent key: ').upper()
+        if p_input == 'I':
+            check_inventory(player)
+            valid = True
+        elif p_input == 'S':
+            #save()
+            valid = True
+        elif p_input == 'E':
+            terminal('Thank you for playing!')
+            sys.exit()
+        else:
+            print('Invalid input try again please')
 def map():
     map_layout = [
         [' ', 'chest', 'chest', 'monster', 'NPC'], 
@@ -37,6 +56,40 @@ def map():
         ['monster', ' ', 'monster', 'chest', 'monster']]
     
     return map_layout # Starting position of player is (0, 0)
+def check_inventory(player):
+    print(f'INVENTORY: {player['inventory']}')
+    valid = False
+    while not valid:
+        if len(player['inventory']) != 0:
+            p_input = input('Would you like to dipose of any items? (Y/N)').lower()
+            if p_input =='y' or p_input =='yes':
+                x = 1
+                for item in player['inventory']:
+                    print(f'{x}. {item}')
+                valid_input = False
+                while not valid_input:
+                    try:
+                        d_input = int(input('Select the corresponding numbered item you would like to remove: '))
+                        if d_input <= len(player['inventory']) and d_input > 0:
+                            player['inventory'].pop(d_input - 1)
+                            print(f'{player['inventory']} has been removed')
+                            print(f'INVENTORY: {player['inventory']}')
+                            valid_input = True
+                        else:
+                            print('That item does not exist try again')
+                    
+                    except ValueError:
+                        print('Wrong input try again')
+                valid = True
+            elif p_input =='n' or p_input =='no':
+                valid = True
+            else:
+                print('Invalid input try again')
+        elif len(player['inventory']) == 0:
+            terminal('A few flies fly out of your empty inventory, perhaps you should start searching')
+            valid = True
+
+                            
 def Movement_option(player):
     terminal('The paths ahead are shrouded in darkness, where will you go?')
     #check if player can move north, south, east or west based on current position and map boundaries
@@ -50,7 +103,7 @@ def Movement_option(player):
     if p_position[1] > 0:
         print('4. Move West')
     try:
-        m_input = int(input('Enter the number corresponding to your desired direction: '))
+        m_input = int(input('Enter the number corresponding to your desired direction: OR  5 to access menu'))
         if m_input == 1 and p_position[0] > 0:
             p_position[0] -= 1
         elif m_input == 2 and p_position[0] < 4:
@@ -59,6 +112,8 @@ def Movement_option(player):
             p_position[1] += 1
         elif m_input == 4 and p_position[1] > 0:
             p_position[1] -= 1
+        elif m_input == 5:
+            menu(player)
         else:
             print("⚠️⚠️You cannot move in that direction, try again⚠️⚠️")
     except ValueError:
@@ -79,7 +134,6 @@ def event(player, tile, map_layout):
         pass
     elif tile == 'chest':
         chest(player)
-        #debug_inventory() need to remove duplicates
         pass
     elif tile == 'trap':
         #trap()
@@ -91,29 +145,54 @@ def chest(player):
              ]
     RNG_item = items[4] 
     #items[random.randint(0, 11)]
-    player['inventory'].append(RNG_item)
     term = f'You have received a {RNG_item}!'
     terminal(term)
-    if RNG_item =='Iron sword':
-        terminal('This item improves your attack by 5')
-        player['attack'] += 5
-    elif RNG_item == 'Cursed blade':
-        terminal('This item improves attack by 15 but lowers HP by 10')
-        player['attack'] += 15
-        player['max_health'] -= 10
-    elif RNG_item == 'Paladin sword':
-        terminal('This item improves attack by 25')
-        player['attack'] += 25
-    elif RNG_item == 'health potion':
-        terminal('This item improves health by 30 when used in combat ')
-        player['healthpotion'] += 1
-    elif RNG_item == 'Giants armour':
-        terminal('This item improves health by 20, permamently')
-        player['max_health'] += 30
-    elif RNG_item == 'dryads cloak':
-        terminal('This item improves health by 30 but lowers attack by 10')
-        player['attack'] -= 10
-        player['max_health'] += 30
+    tally = 0
+    for i in player['inventory']: #check if duplicates exists
+        if RNG_item == i:
+            tally += 1
+    if tally == 0:
+        if RNG_item =='Iron sword':
+            terminal('This item improves your attack by 5')
+            player['attack'] += 5
+        elif RNG_item == 'Cursed blade':
+            terminal('This item improves attack by 15 but lowers HP by 10')
+            player['attack'] += 15
+            player['max_health'] -= 10
+        elif RNG_item == 'Paladin sword':
+            terminal('This item improves attack by 25')
+            player['attack'] += 25
+        elif RNG_item == 'health potion':
+            terminal('This item improves health by 30 when used in combat ')
+            player['healthpotion'] += 1
+        elif RNG_item == 'Giants armour':
+            terminal('This item improves health by 20, permamently')
+            player['max_health'] += 30
+        elif RNG_item == 'dryads cloak':
+            terminal('This item improves health by 30 but lowers attack by 10')
+            player['attack'] -= 10
+            player['max_health'] += 30
+        player['inventory'].append(RNG_item)
+        check_inventory(player)
+    else:
+        terminal('You already have this item so you throw it away')
+        debug_inventory(player) #need to remove duplicates
+
+def debug_inventory(player):
+    inventory_dict = {}
+    for i in player['inventory']:
+        inventory_dict[i] = 0
+    for i in player['inventory']:
+        if i in player['inventory']:
+            inventory_dict[i] += 1
+    for i in player['inventory']:
+        if inventory_dict[i] > 1:
+            player['inventory'].remove(i)
+    print(player['inventory'])
+            
+
+
+
 
 def combat(player):
     e_enemies = ['goblin', 'Silver wolf', 'Troll', 'golem'] #Easy enemies
@@ -200,11 +279,17 @@ def combat(player):
                     terminal('Failed to escape combat will resume')
 
             print(f'player ❤️: {player['health']}/{player['max_health']}, player ⚔️ {player['attack'] * damage_bonus}, monster ❤️ {monster['health']}')
-        if player['health'] < 0:
+        if player['health'] <= 0:
             a = f'Game over {player['name']}, You have died to the {encounter}'
             terminal(a)
             #delete_save()
             sys.exit()
+        elif player['health'] > 0 and monster['health'] <= 0:
+            chest_RNG = random.randint(0, 100)
+            if chest_RNG <= 80 and chest_RNG >= 0:
+                terminal('The opponent has dropped something!!')
+                terminal('rolling..........')
+                chest(player)
     elif player['attack'] > 35:
         h_enemies = ['Wyvern', 'Centaur', 'Hydra', 'Phoenix']
         encounter = h_enemies[random.randint(0, 3)]
@@ -261,11 +346,11 @@ def combat(player):
                 except:
                     print('Wrong input try again')
             if combat_option == 1:
-                monster['health'] -= player['attack'] * damage_bonus
-                print(f'{player['name']} has dealt {player['attack'] * damage_bonus} to {encounter}')
+                monster['health'] -= round(player['attack'] * damage_bonus)
+                print(f'{player['name']} has dealt {round(player['attack'] * damage_bonus, 2)} to {encounter}')
                 time.sleep(1.5) # AI
-                print(f'{encounter} fights back and has dealt {monster['damage']} damage to {player['name']}')
-                player['health'] -= monster['damage']
+                print(f'{encounter} fights back and has dealt {round(monster['damage'], 2)} damage to {player['name']}')
+                player['health'] -= round(monster['damage'], 2)
             elif combat_option == 2:
                 if player['healthpotion'] >= 1:
                     terminal('You have use a health potion and restored +30 HP')
@@ -292,6 +377,12 @@ def combat(player):
             terminal(a)
             #delete_save()
             sys.exit()
+        elif player['health'] > 0 and monster['health'] <= 0:
+            chest_RNG = random.randint(0, 100)
+            if chest_RNG <= 80 and chest_RNG >= 0:
+                terminal('The opponent has dropped something!!')
+                terminal('rolling..........')
+                chest(player)
         
         
             
